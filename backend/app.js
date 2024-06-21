b85bd94e95ab08a5586376aaa711e09d7c114015
Alireza Ghanbari <alirezaghanbari.dev@gmail.com>
2024-06-21 22:59:24 +0430

    1-

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+import express from "express"
+import morgan from "morgan"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app = express()
+app.use(morgan("dev"))
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
+
+app.get("/", (req, res)=>{
+    res.send("server is ready")
+})
+
+//Routes
+// app.use('/api/v1/auth', authRouter)
+
+export default app
